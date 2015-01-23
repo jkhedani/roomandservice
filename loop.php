fb27b543e50fe89a0d9e164696447e46bcfab8e5
Justin Hedani <jkhedani@gmail.com>
2015-01-22 16:44:03 -1000

    Limit character counts on articles

--- loop.php
+++ loop.php
@@ -1,12 +1,12 @@
 <?php if (have_posts()): while (have_posts()) : the_post(); ?>
-	
+
 	<?php if(is_category( 'eat' ) || is_category( 'shop' ) || is_category( 'play' )) : ?>
 		<!-- article -->
 		<article id="post-<?php the_ID(); ?>" <?php post_class('three-col-rtl'); ?>>
 
 			<!-- post title -->
 			<h2 class="post-title-grid">
-				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php echo limit_character_count( get_the_title(), 23); ?></a>
 			</h2>
 			<!-- /post title -->
 
@@ -20,15 +20,17 @@
 
 			<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
 
-		</article>   
-	
+			<a class="editorial-more" href="<?php echo get_the_permalink(); ?>">+ Read More</a>
+
+		</article>
+
 	<?php  else: ?>
 		<!-- article -->
 			<article id="post-<?php the_ID(); ?>" <?php post_class('three-col'); ?>>
 
 				<!-- post title -->
 				<h2 class="post-title-grid">
-					<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+					<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php echo limit_character_count( get_the_title(), 23); ?></a>
 				</h2>
 				<!-- /post title -->
 
@@ -40,7 +42,9 @@
 				<?php endif; ?>
 				<!-- /post thumbnail -->
 
-				<?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+				<?php html5wp_excerpt('html5wp_index', 100); // Build your custom callback length in functions.php ?>
+
+				<a class="editorial-more" href="<?php echo get_the_permalink(); ?>">+ Read More</a>
 
 			</article>
 			<!-- /article -->
